efee22bf8de24c34ddfe9aaaae0e841d03acd2e0
Talin <talin@viridia.org>
2011-04-01 18:22:31 -0700

    Improved minimal compilation scripts.

--- trunk/scripts/link-minimal.sh
+++ trunk/scripts/link-minimal.sh
@@ -2,4 +2,4 @@
 
 PRGNAME=${1%.*}
 
-llc -O0 -disable-fp-elim -filetype=obj $PRGNAME.ll && /usr/bin/c++ -g -o $PRGNAME $PRGNAME.o 
+llc -O0 -disable-fp-elim -filetype=obj $PRGNAME.ll && /usr/bin/c++ -g -o $PRGNAME $PRGNAME.o runtime/libruntime.a
