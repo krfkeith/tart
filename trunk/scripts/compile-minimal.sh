acc40801858972449d05025e11d4a84d22823595
Talin <talin@viridia.org>
2011-04-01 18:22:31 -0700

    Improved minimal compilation scripts.

--- trunk/scripts/compile-minimal.sh
+++ trunk/scripts/compile-minimal.sh
@@ -1,12 +1,16 @@
 #/bin/bash
 
+SCRIPTDIR=`dirname $0`
+
 PRGNAME=${1%.tart}
 
 TARTC=tools/tartc/tartc
-STDLIB=/home/talin/Projects/tart/trunk/lib/std
+STDLIB=`pwd`/$(dirname $(dirname $0))/lib/std
 SRCPATH=$PWD
 
 $TARTC -nogc -g -i $STDLIB -noreflect -sourcepath $SRCPATH $PRGNAME.tart &&\
   llvm-dis -o - $PRGNAME.bc \
   | sed "s/%tart.core.TypeInfoBlock\* bitcast ([^)]\+)/%tart.core.TypeInfoBlock* null/g" \
+  | sed "s/external constant %0/constant %0 zeroinitializer/g" \
   > $PRGNAME.ll
+
